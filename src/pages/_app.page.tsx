a3f5343d51de97da109a55dc236945fab2c5e7a4
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-03-06 15:53:22 -0300

    add: calendar login using Next Auth and Google Calendar API

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,7 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
+import { SessionProvider } from 'next-auth/react'
 
 globalStyles()
 
@@ -10,10 +11,15 @@ const roboto = Roboto({
   variable: '--font-roboto',
 })
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <main className={roboto.className}>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </main>
   )
 }
